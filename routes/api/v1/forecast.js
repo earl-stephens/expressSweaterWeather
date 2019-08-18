aac2f4a130a9d2dbd5dab69c74a221dd1730a6da
earl-stephens <stephensearl@hotmail.com>
2019-08-17 22:18:14 -0600

    Set up the dotenv package and the node-fetch package in preparation for making api requests

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -4,19 +4,21 @@ var User = require('../../../models').User;
 var bcrypt = require('bcrypt');
 var saltRounds = 10;
 var randomstring = require('randomstring');
+require('dotenv').config();
+var fetch = require('node-fetch');
 
 // GET forecast for a city
 router.get('/', function(req, res, next) {
   User.findAll({where: {api_key: req.body.api_key}})
   .then(user => {
-    console.log(req.params);
     if (user != null) {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).send();//.send(JSON.stringify(weatherResponse));
-  } else {
-    res.setHeader("Content-Type", "application/json");
-    res.status(401).send("Unauthorized");
-  }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).send();//.send(JSON.stringify(weatherResponse));
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.status(401).send("Unauthorized");
+    }
   })
   .catch(error => {
     res.setHeader("Content-Type", "application/json");
